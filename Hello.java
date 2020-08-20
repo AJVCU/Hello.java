e6a100e7796909fb97d233e09f9c276cf522d37c
AJVCU <69979225+AJVCU@users.noreply.github.com>
2020-08-20 11:44:26 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*************************************************************
+* Hello.java
+**************************************************************
+* Avery James
+* 8-20-2020
+* CMSC 255 001
+*************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Avery James");
+		System.out.println("Hello.java");
+		System.out.println("This lab will help you get started with Java.  You will make sure it is properly installed and loaded on your computer and that you can run a program in Java.");
+		System.out.println("8/20/2020");
+		System.out.println("CMSC 255 001");
+		System.out.println();
+		}
+	}
